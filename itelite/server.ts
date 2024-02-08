1f8e6724e87700a480371008f0bf23fe8b47f493
Lukasz7h <luuukasz368@gmail.com>
2024-02-08 10:59:36 +0100

    test ssr

--- itelite/server.ts
+++ itelite/server.ts
@@ -44,12 +44,12 @@ export function app(): express.Express {
 }
 
 function run(): void {
-  const port = 'http://itelite-website-hqvz6990m-itelite.vercel.app';
+  const port = process.env['API_URL'];
 
   // Start up the Node server
   const server = app();
   server.listen(port, () => {
-    console.log(`Node Express server listening on http://itelite-website-hqvz6990m-itelite.vercel.app`);
+    console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
 
