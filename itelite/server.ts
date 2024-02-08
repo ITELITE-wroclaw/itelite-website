730672fc071200f32de0f09fd1a5d50825480f19
Lukasz7h <luuukasz368@gmail.com>
2024-02-08 10:43:41 +0100

    build with vercel url

--- itelite/server.ts
+++ itelite/server.ts
@@ -49,7 +49,7 @@ function run(): void {
   // Start up the Node server
   const server = app();
   server.listen(port, () => {
-    console.log(`Node Express server listening on http://${process.env['API_URL']}`);
+    console.log(`Node Express server listening on http://itelite-website-hqvz6990m-itelite.vercel.app`);
   });
 }
 
